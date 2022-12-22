cd08fd73df897754cc9d743dd910715d68f7c2f3
Owen2045 <sam24654113@gmail.com>
2022-12-22 17:32:37 +0800

    新增指令!

---
+++ Discord.py
@@ -0,0 +1,82 @@
+import discord
+import os
+import random
+import interactions
+from dotenv import load_dotenv
+from discord.ext import commands
+from pretty_help import PrettyHelp, EmojiMenu
+
+load_dotenv()
+# 環境變數
+TOKEN = os.getenv('The_Crane_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+# client 是我們與 Discord 連結的橋樑
+intents = discord.Intents.default()
+# intents = 權限
+intents.members = True
+intents.message_content = True
+intents.messages = True
+client = discord.Client(intents=intents)
+
+# bot = commands.Bot(command_prefix="!", intents=intents, help_command=PrettyHelp())
+bot = interactions.Client(command_prefix='!', token=TOKEN)
+
+# slash = interactions.SlashCommand(bot, sync_commands=True)
+
+@client.event
+# bot 啟動設定訊息
+async def on_ready():
+    print('目前登入身份：', client.user)
+    game = discord.Game('與你媽在SWAG上見面')
+    await client.change_presence(status=discord.Status.online, activity=game)
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+    print(f'{guild.name}(id: {guild.id})')
+
+# 加入伺服器通知
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f' {member.name}, welcome to hell'
+    )
+
+
+# @bot.command(name='roll_dice', help='Simulates rolling dice.')
+# async def roll(ctx, number_of_dice: int, number_of_sides: int):
+#     dice = [
+#         str(random.choice(range(1, number_of_sides + 1)))
+#         for _ in range(number_of_dice)
+#     ]
+#     await ctx.send(', '.join(dice))
+
+
+
+
+
+@client.event
+# 訊息事件
+async def on_message(message):
+    # 排除自己的訊息，避免陷入無限循環
+    if message.author == client.user:
+        return
+    #如果包含 ping，機器人回傳 pong
+    if message.content == 'ping':
+        await message.channel.send('pog')
+
+    if message.content.startswith('史粉'):
+        channel = message.channel
+        #機器人叫你先跟他說你好
+        await channel.send('逼逼逼...績優蒿難喝...')
+        #檢查函式，確認使用者是否在相同頻道打上「你好」
+        def checkmessage(m):
+            return m.content == '你好' and m.channel == channel
+        #獲取傳訊息的資訊(message是類型，也可以用reaction_add等等動作)
+        msg = await client.wait_for('message', check=checkmessage)
+        await channel.send('嗨, {.author}!'.format(msg))
+
+
+client.run(TOKEN) #TOKEN 在剛剛 Discord Developer 那邊「BOT」頁面裡面
+# bot.start()
