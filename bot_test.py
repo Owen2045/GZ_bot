c1509c0ebcc45b0ee51082f615ffeec51c8c96bf
Owen2045 <sam24654113@gmail.com>
2023-01-04 20:51:07 +0800

    爬蟲解決

--- bot_test.py
+++ bot_test.py
@@ -12,11 +12,14 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE',  'GZ_bot.settings')
 
 
 
-
-url = 'https://gamezbd-info.pages.dev/timer'
-request_site = Request(url, headers={"User-Agent": "Mozilla/5.0"})
-webpage = urlopen(request_site).read()
-soup = BeautifulSoup(webpage, "html.parser")
-mydivs = soup.find(id_='q-app')
-print(mydivs)
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+browser = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+browser.get('https://gamezbd-info.pages.dev/timer')
+div3 = browser.find_element(By.XPATH,'//*[@id="q-app"]/div/div[2]/div[1]/main/div/div[2]/div')
+print(div3.text)
 
